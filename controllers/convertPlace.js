5e70788e79cd24befc2bdb90981c0ecefd470478
aboudHasan <aboudhasan06@gmail.com>
2025-11-23 01:37:15 -0500

    added new endpoint to convert lat and lon to place object

--- controllers/convertPlace.js
+++ controllers/convertPlace.js
@@ -22,3 +22,29 @@ export const convertPlace = async (place) => {
     throw error;
   }
 };
+// https://api.geoapify.com/v1/geocode/reverse?lat=52.51894887928074&lon=13.409808180753316&format=json&apiKey=YOUR_API_KEY
+
+export const convertCoords = async (req, res, next) => {
+  try {
+    const { lat, lon } = req.query;
+
+    if (!lat || !lon) {
+      const error = new Error("Must include both lat and lon");
+      error.status = 400;
+      throw error;
+    }
+
+    let placeInformation = await fetch(
+      `https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lon}&format=json&apiKey=${apiKey}`
+    );
+
+    if (!placeInformation.ok) {
+      throw new Error("API error");
+    }
+
+    placeInformation = await placeInformation.json();
+    res.status(200).json(placeInformation);
+  } catch (error) {
+    next(error);
+  }
+};
--- routes/router.js
+++ routes/router.js
@@ -2,6 +2,7 @@ import express from "express";
 import { fetchByBBox, fetchByName } from "../controllers/places.js";
 import { fetchTile, fetchTilesByPlace } from "../controllers/tiles.js";
 import { getEnemiesFromTile } from "../controllers/enemies.js";
+import { convertCoords } from "../controllers/convertPlace.js";
 
 const router = express.Router();
 
@@ -10,5 +11,6 @@ router.get("/name", fetchByName);
 router.get("/tile-data", fetchTile);
 router.get("/tiles-by-place", fetchTilesByPlace);
 router.get("/enemy-tile", getEnemiesFromTile);
+router.get("/convert-place", convertCoords);
 
 export default router;
